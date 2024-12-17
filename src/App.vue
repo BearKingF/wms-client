24ed17e17c6791710dad7043b79a8e9e0c8e1c7b
bearking <1678801076@qq.com>
2024-12-17 23:39:26 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style>
+
+/*顶部进度条样式*/
+#nprogress .bar {
+  background: #1890ff !important;
+}
+
+body {
+  letter-spacing: 1px;
+  background: #f0f2f5 !important;
+  height: 100%;
+}
+
+#app{
+  height: 100%;
+}
+
+</style>
---
+++ src/layout/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <a-layout-footer :style="{ textAlign: 'center', letterSpacing: '1px' }">
+    <a href="https://space.bilibili.com/1756783214/channel/series" target='_blank'><font color=red>哈喽瓦德收藏整理</font></a>
+  </a-layout-footer>
+</template>
+
+<script>
+export default {
+name: "Footer"
+}
+</script>
+
+<style scoped>
+
+</style>
